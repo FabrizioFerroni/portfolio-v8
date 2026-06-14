bad8f44cea106fc936c6d9b94cf364fadf5f0dc8
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2026-06-14 13:50:31 -0300

    chore: config inicial

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+// eslint.config.js
+const angular = require('angular-eslint');
+const tseslint = require('typescript-eslint');
+const eslint = require('@eslint/js');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['projects/**/*'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended, // equivalente al .eslintrc original
+    ],
+    processor: angular.processInlineTemplates,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  }
+);
