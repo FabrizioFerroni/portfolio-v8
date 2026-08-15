bd8e0e105a161e757bef24e144f08ea9e629c3e6
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2026-08-15 16:14:44 -0300

    fix: fix proxy main

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import './imports-prims';
 
+(globalThis as unknown as { process: unknown }).process = { env: {} };
+
 bootstrapApplication(App, appConfig).catch(err => console.error(err));
